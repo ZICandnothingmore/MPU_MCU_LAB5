1d4f3821d70726e7d1babe89c178bac978f61a6b
ZICandnothingmore <baonhu0508@gmail.com>
2024-11-30 06:59:57 +0700

    fsm for parser command

---
+++ Code/Core/Inc/global.h
@@ -0,0 +1,48 @@
+/*
+ * global.h
+ *
+ *  Created on: 30 thg 11, 2024
+ *      Author: ACER
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include "main.h"
+
+// FOR PARSER FSM
+#define INIT	0
+#define _		1
+#define _R		2
+#define _RS		3
+#define _RST	4
+#define _RST_	5
+
+#define O			11
+#define OK			12
+
+// FOR COMMUNICATION FSM
+#define SEND		21
+#define WAIT		22
+
+// FOR UART
+#define MAX_BUFFER_SIZE 30
+
+extern uint8_t temp;
+#define MAX_BUFFER_SIZE	30
+
+extern uint8_t temp;
+extern uint8_t buffer[MAX_BUFFER_SIZE];
+extern uint8_t index_buffer;
+extern uint8_t buffer_flag;
+extern int parser_check_next;
+extern int message_UART;
+extern uint8_t command_flag;
+extern uint8_t command_data[MAX_BUFFER_SIZE];
+
+extern int BEGIN;
+extern int END;
+extern uint8_t index_buffer;
+extern uint8_t buffer_flag;
+
+#endif /* INC_GLOBAL_H_ */
---
+++ Code/Core/Src/global.c
@@ -0,0 +1,22 @@
+/*
+ * global.c
+ *
+ *  Created on: 30 thg 11, 2024
+ *      Author: ACER
+ */
+
+#include "global.h"
+
+uint8_t temp = 0;
+uint8_t buffer[MAX_BUFFER_SIZE];
+uint8_t index_buffer = 0;
+uint8_t buffer_flag = 0;
+int parser_check_next = INIT;
+int message_UART;
+uint8_t command_flag = 1;
+uint8_t command_data[MAX_BUFFER_SIZE];
+
+int BEGIN = 1;
+int END = 0;
+
+
